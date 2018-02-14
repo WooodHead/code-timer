557f41d0c50b271739e3b076e6e58f7b632b5ee0
gbatra17 <gbatra17@gmail.com>
2018-02-14 01:17:19 -0600

    Changed title name and added description

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,8 @@ class Header extends Component {
     return (
       <div className="App">
       <h1 className="App-title">Code-Timer</h1>
+      <h2>A text-based app that sends you daily notifications on the number of commits you made and time spent coding.</h2>
+      <h3>Made using WakaTime and Twilio API</h3>
     </div>);
   }
 }
