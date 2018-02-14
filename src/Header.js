604dbc30777fc69b6067257cab70defb7f44d88b
gbatra17 <gbatra17@gmail.com>
2018-02-14 00:40:16 -0600

    Added a header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1 className="App-title">Code-Timer</h1>
+    </div>);
+  }
+}
+
+export default Header;
