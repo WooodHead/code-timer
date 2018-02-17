0ca101c0e8d743ff8ed69660207de2ebd5a5c867
gbatra17 <gbatra17@gmail.com>
2018-02-16 23:32:54 -0600

    Working on 0auth implementation

--- server/index.js
+++ server/index.js
@@ -1,35 +1,40 @@
 const express = require('express');
-const simpleOauthModule = require('./../');
+const simpleOauthModule = require('simple-oauth2');
+const passwords = require('../passwords.js');
 
 const app = express();
+
+//app.use(express.static(__dirname + '/../build'));
+
 const oauth2 = simpleOauthModule.create({
   client: {
-    id: '<CLIENT_ID>',
-    secret: '<CLIENT_SECRET>',
+    id: passwords.WAKATIME_CLIENT_ID,
+    secret: passwords.WAKATIME_CLIENT_SECRET
   },
   auth: {
-    tokenHost: 'https://github.com',
-    tokenPath: '/login/oauth/access_token',
-    authorizePath: '/login/oauth/authorize',
+    tokenHost: 'https://wakatime.com',
+    tokenPath: '/oauth/token',
+    authorizePath: '/oauth/authorize',
   },
 });
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'http://localhost:3000/callback',
-  scope: 'notifications',
-  state: '3(#0/!~',
+  redirect_uri: 'https://wakatime.com/oauth/test',
+  response_type: 'code',
+  scope: 'email,read_stats'
 });
 
-// Initial page redirecting to Github
+// Initial page redirecting to WakaTime
 app.get('/auth', (req, res) => {
-  console.log(authorizationUri);
+  console.log('this is the authurl', authorizationUri);
   res.redirect(authorizationUri);
 });
 
 // Callback service parsing the authorization token and asking for the access token
 app.get('/callback', (req, res) => {
   const code = req.query.code;
+  console.log(code);
   const options = {
     code,
   };
@@ -43,9 +48,7 @@ app.get('/callback', (req, res) => {
     console.log('The resulting token: ', result);
     const token = oauth2.accessToken.create(result);
 
-    return res
-      .status(200)
-      .json(token);
+    return res.status(200).json(token);
   });
 });
 
@@ -53,9 +56,9 @@ app.get('/success', (req, res) => {
   res.send('');
 });
 
-// app.get('/', (req, res) => {
-//   res.send('Hello<br><a href="/auth">Log in with Github</a>');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello<br><a href="/auth">Log in with WakaTime</a>');
+});
 
 app.listen(8080, () => {
   console.log('Express server started on port 8080'); // eslint-disable-line
